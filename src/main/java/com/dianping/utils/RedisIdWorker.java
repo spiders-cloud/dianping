d3e79ed64fb38068e383c7128da4cf6637637d3d
zhaoz <1208390864@qq.com>
2025-04-26 21:52:30 +0800

    feat: redis生成全局统一ID

---
+++ src/main/java/com/dianping/utils/RedisIdWorker.java
@@ -0,0 +1,44 @@
+package com.dianping.utils;
+
+import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.stereotype.Component;
+
+import java.time.LocalDateTime;
+import java.time.ZoneOffset;
+import java.time.format.DateTimeFormatter;
+
+/**
+ * @Description: 全局ID生成器
+ * @Author: zhao
+ * Created: 2025/4/26 - 21:19
+ */
+@Component
+public class RedisIdWorker {
+
+    /** 开始时间戳 */
+    private static final long BEGIN_TIMESTAMP = 1672531200L;
+    /** 序列号的位数 */
+    private static final int COUNT_BITS = 32;
+
+    private final StringRedisTemplate stringRedisTemplate;
+
+    public RedisIdWorker(StringRedisTemplate stringRedisTemplate) {
+        this.stringRedisTemplate = stringRedisTemplate;
+    }
+
+    public long nextId(String keyPrefix) {
+        // 1.生成时间戳
+        LocalDateTime now = LocalDateTime.now();
+        long nowSecond = now.toEpochSecond(ZoneOffset.UTC);
+        long timestamp = nowSecond - BEGIN_TIMESTAMP;
+
+        // 2.生成序列号
+        // 2.1.获取当前日期，精确到天
+        String date = now.format(DateTimeFormatter.ofPattern("yyyy:MM:dd"));
+        // 2.2.自增长
+        long count = stringRedisTemplate.opsForValue().increment("icr:" + keyPrefix + ":" + date);
+
+        // 3.拼接并返回
+        return timestamp << COUNT_BITS | count;
+    }
+}
