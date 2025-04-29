0be29b9de44314338df6ff950766275b2a675809
zhaoz <1208390864@qq.com>
2025-04-29 20:39:18 +0800

    feat: redis分布式锁V3-Lua脚本保证删锁的原子性

--- src/main/java/com/dianping/service/impl/SimpleRedisLock.java
+++ src/main/java/com/dianping/service/impl/SimpleRedisLock.java
@@ -3,8 +3,11 @@ package com.dianping.service.impl;
 import cn.hutool.core.lang.UUID;
 import cn.hutool.core.util.BooleanUtil;
 import com.dianping.service.ILock;
+import org.springframework.core.io.ClassPathResource;
 import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.data.redis.core.script.DefaultRedisScript;
 
+import java.util.Collections;
 import java.util.concurrent.TimeUnit;
 
 /**
@@ -14,6 +17,16 @@ import java.util.concurrent.TimeUnit;
  */
 public class SimpleRedisLock implements ILock {
 
+    private static final String KEY_PREFIX = "lock:";
+    private static final String ID_PREFIX = UUID.randomUUID().toString(true) + "-";
+    private static final DefaultRedisScript<Long> UNLOCK_SCRIPT;
+
+    static {
+        UNLOCK_SCRIPT = new DefaultRedisScript<>();
+        UNLOCK_SCRIPT.setLocation(new ClassPathResource("unlock.lua"));
+        UNLOCK_SCRIPT.setResultType(Long.class);
+    }
+
     private final String lockName;
     private final StringRedisTemplate stringRedisTemplate;
 
@@ -22,21 +35,23 @@ public class SimpleRedisLock implements ILock {
         this.stringRedisTemplate = stringRedisTemplate;
     }
 
-    private static final String KEY_PREFIX = "lock:";
-    private static final String ID_PREFIX = UUID.randomUUID().toString(true) + "-";
-
     @Override
     public boolean tryLock(long timeoutSec) {
         // 获取线程标识
         String threadId = ID_PREFIX + Thread.currentThread().getId();
         // 获取锁
-        Boolean success = stringRedisTemplate.opsForValue()
-                                             .setIfAbsent(KEY_PREFIX + lockName, threadId, timeoutSec,
-                                                     TimeUnit.SECONDS);
+        Boolean success = stringRedisTemplate.opsForValue().setIfAbsent(KEY_PREFIX + lockName, threadId, timeoutSec,
+                                                                        TimeUnit.SECONDS);
         return BooleanUtil.isTrue(success);
     }
 
     @Override
+    public void unlock() {
+        // 调用Lua脚本
+        stringRedisTemplate.execute(UNLOCK_SCRIPT, Collections.singletonList(KEY_PREFIX + lockName),
+                                    ID_PREFIX + Thread.currentThread().getId());
+    }
+    /* @Override
     public void unlock() {
         // 获取线程标示
         String threadId = ID_PREFIX + Thread.currentThread().getId();
@@ -47,5 +62,5 @@ public class SimpleRedisLock implements ILock {
             // 释放锁
             stringRedisTemplate.delete(KEY_PREFIX + lockName);
         }
-    }
+    } */
 }
